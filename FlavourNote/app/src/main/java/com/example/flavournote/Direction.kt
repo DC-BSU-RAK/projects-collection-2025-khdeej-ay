b8275ffb524e37fc1bb73bfbafe7a100d0ac89c8
Khadeeja <s.khdeej@gmail.com>
2025-05-25 01:55:26 +0400

    Create classes

---
+++ FlavourNote/app/src/main/java/com/example/flavournote/Direction.kt
@@ -0,0 +1,5 @@
+package com.example.flavournote
+
+import java.io.Serializable
+
+data class Direction(val step: String) : Serializable
---
+++ FlavourNote/app/src/main/java/com/example/flavournote/Ingredient.kt
@@ -0,0 +1,5 @@
+package com.example.flavournote
+
+import java.io.Serializable
+
+data class Ingredient(val name: String, val quantity: String) : Serializable
